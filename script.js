d12466b2b88bfb20793c1b4fb15441584b45b812
abubakarim78 <abubakarim78@gmail.com>
2026-01-06 23:51:43 +0000

    javascript built

---
+++ script.js
@@ -0,0 +1,22 @@
+// Defining all variables 
+
+const notesContainer = document.querySelector(".notes-container");
+const createBtn = document.querySelector(".btn");
+let notes = document.querySelectorAll(".input-box");
+
+
+createBtn.addEventListener("click", ()=>{
+    let inputBox = document.createElement("p");
+    img = document.createElement("img");
+    inputBox.className = "input-box";
+    inputBox.setAttribute("contenteditable", "true");
+    img.src = "images/delete.png";
+
+    notesContainer.appendChild(inputBox).appendChild(img);
+})
+
+notesContainer.addEventListener("click", function(e){
+    if(e.target.tagName === "IMG"){
+        e.target.parentElement.remove();
+    };
+})
